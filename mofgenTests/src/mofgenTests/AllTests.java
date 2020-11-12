43a35f3c64630faaa8463e8ae0819fd37b3c1626
tobnie <niehues.tobias@gmail.com>
2020-11-12 18:17:45 +0100

    added testsuite

---
+++ mofgenTests/src/mofgenTests/AllTests.java
@@ -0,0 +1,11 @@
+package mofgenTests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CollectionsTest.class, GeneralTest.class })
+public class AllTests {
+
+}
